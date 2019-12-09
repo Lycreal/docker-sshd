2f1016faa6e25c8310cb7f2114ff83b12c8016dc
Lycreal <jgsly123@gmail.com>
2019-12-09 16:56:44 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,21 @@
 
 echo $key > /root/.ssh/authorized_keys
 
+while [ $users ]
+do
+  user=$(echo $users | cut -d ',' -f1)
+  users=${users#*$user}
+  users=${users#*,}
+  u=$(echo $user | cut -d ':' -f1)
+  useradd -m -s /bin/bash $u
+  if [[ $user == *":"* ]];then
+	echo $user|chpasswd
+  else
+    echo $u:$u|chpasswd
+  fi
+done
+
+
 if [ $# -ne 0 ];then
     /bin/sh -c "$*"
 fi
