0551edb2a5c996936e3a42005b32b15542869c10
Lycreal <jgsly123@gmail.com>
2019-12-10 20:06:14 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo $key > /root/.ssh/authorized_keys
+echo $key >> /root/.ssh/authorized_keys
 
 if [ $port ];then
   sed -r "s/#?(Port).*/\1 ${port}/" -i /etc/ssh/sshd_config
@@ -9,19 +9,19 @@ fi
 while [ $users ]
 do
   user=$(echo $users | cut -d ',' -f1)
-  users=${users#*$user}
-  users=${users#*,}
   u=$(echo $user | cut -d ':' -f1)
   useradd -m -s /bin/bash $u
   if [[ $user == *":"* ]];then
-	echo $user|chpasswd
+    echo $user|chpasswd
   else
     echo $u:$u|chpasswd
   fi
+  users=${users#*$user}
+  users=${users#*,}
 done
 
 if [ $# -ne 0 ];then
-    /bin/bash -c '$*'
+    /bin/bash -c "$*"
 fi
 
 while [ true ]; do
